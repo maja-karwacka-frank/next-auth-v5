72156b482556fe2c95d3ad8a8a16887d2d7183e4
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2024-01-08 14:31:28 +0100

    feat: home page & card wrapper

---
+++ components/auth/login-form.tsx
@@ -0,0 +1,13 @@
+import { CardWrapper } from './card-wrapper';
+
+export const LoginForm = () => {
+	return (
+		<CardWrapper
+			headerLabel='Welcome Back'
+			backButtonLabel="Don't have an account?"
+			backButtonHref='/auth/register'
+			showSocial>
+			Login Form!
+		</CardWrapper>
+	);
+};
