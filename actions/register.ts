454d7911e33d762263a33d7e79c7e96ccf2316cf
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2024-01-27 12:58:52 +0100

    Update login and registration logic

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,7 @@
 'use server';
 
 import * as z from 'zod';
-import bcrypt from 'bcrypt';
+import bcryptjs from 'bcryptjs';
 
 import { db } from '@/lib/db';
 import { RegisterSchema } from '@/schemas';
@@ -16,7 +16,7 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
 
 	const { email, password, name } = validatedFields.data;
 
-	const hashedPassword = await bcrypt.hash(password, 10);
+	const hashedPassword = await bcryptjs.hash(password, 10);
 
 	const existingUser = await getUserByEmail(email)
 
