388061f32839d13176fe0ae7eee428d367900caf
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2024-01-27 14:12:52 +0100

    feat: next auth callbacks

--- auth.ts
+++ auth.ts
@@ -3,6 +3,8 @@ import { PrismaAdapter } from '@auth/prisma-adapter';
 
 import { db } from '@/lib/db';
 import authConfig from '@/auth.config';
+import { getUserById } from '@/data/user';
+import { UserRole } from '@prisma/client';
 
 export const {
 	handlers: { GET, POST },
@@ -10,7 +12,31 @@ export const {
 	signIn,
 	signOut,
 } = NextAuth({
-    adapter: PrismaAdapter(db),
-    session: { strategy: 'jwt' },
+	callbacks: {
+		async session({ token, session }) {
+			if (token.sub && session.user) {
+				session.user.id = token.sub;
+			}
+
+			if (token.role && session.user) {
+				session.user.role = token.role as UserRole;
+			}
+
+			return session;
+		},
+		async jwt({ token }) {
+			if (!token.sub) return token;
+
+			const existingUser = await getUserById(token.sub);
+
+			if (!existingUser) return token;
+
+			token.role = existingUser.role;
+
+			return token;
+		},
+	},
+	adapter: PrismaAdapter(db),
+	session: { strategy: 'jwt' },
 	...authConfig,
 });
