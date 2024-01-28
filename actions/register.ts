aea4403ae7c16a67895710ded5942382ec4f8fbe
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2024-01-28 14:23:48 +0100

    feat: email verification

--- actions/register.ts
+++ actions/register.ts
@@ -6,6 +6,8 @@ import bcryptjs from 'bcryptjs';
 import { db } from '@/lib/db';
 import { RegisterSchema } from '@/schemas';
 import { getUserByEmail } from '@/data/user';
+import { generateVerificationToken } from '@/lib/tokens';
+import { sendVerificationEmail } from '@/lib/mail';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
 	const validatedFields = RegisterSchema.safeParse(values);
@@ -18,7 +20,7 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
 
 	const hashedPassword = await bcryptjs.hash(password, 10);
 
-	const existingUser = await getUserByEmail(email)
+	const existingUser = await getUserByEmail(email);
 
 	if (existingUser) {
 		return { error: 'Email already in use!' };
@@ -32,7 +34,9 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
 		},
 	});
 
-	// TODO: Send verification token email
+	const verificationToken = await generateVerificationToken(email);
 
-	return { success: 'User created!' };
+	await sendVerificationEmail(verificationToken.email, verificationToken.token);
+
+	return { success: 'Confirmation email sent!' };
 };
