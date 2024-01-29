de36c38703dc3f92b511a2b072a92daeca5af3a8
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2024-01-29 16:06:01 +0100

    feat: user button

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,19 +1,37 @@
-import { auth, signOut } from '@/auth';
+'use client';
 
-const SettingPage = async () => {
-	const session = await auth();
+import { logout } from '@/actions/logout';
+import { useCurrentUser } from '@/hooks/use-current-user';
+// import { useSession, signOut } from 'next-auth/react';
+
+// import { auth, signOut } from '@/auth';
+
+const SettingPage = () => {
+	// const session = await auth(); // on server side with async page
+	// const session = useSession(); // on client side
+	const user = useCurrentUser();
+
+	const onClick = () => {
+		// signOut(); // on client side
+		logout();
+	};
 
 	return (
-		<div>
-			{JSON.stringify(session)}
-			<form
-				action={async () => {
-					'use server';
-					await signOut();
-				}}>
-				<button type='submit'>Sign out</button>
-			</form>
-		</div>
+		<>
+			{/* <form
+		action={async () => {
+			'use server';
+			await signOut();
+		}}>
+		<button type='submit'>Sign out</button>
+	</form> */}
+
+			<div className='bg-white p-10 rounded-xl'>
+				<button type='submit' onClick={onClick}>
+					Sign out
+				</button>
+			</div>
+		</>
 	);
 };
 
