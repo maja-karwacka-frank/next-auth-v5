868d32cd10f7441bb2821c907de7cd930aa7ff30
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2024-01-31 08:58:04 +0100

    feat: settings action

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,37 +1,96 @@
 'use client';
 
-import { logout } from '@/actions/logout';
-import { useCurrentUser } from '@/hooks/use-current-user';
-// import { useSession, signOut } from 'next-auth/react';
+import * as z from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useTransition, useState } from 'react';
+import { useSession } from 'next-auth/react';
 
-// import { auth, signOut } from '@/auth';
+import { settings } from '@/actions/settings';
+import { Button } from '@/components/ui/button';
+import { SettingsSchema } from '@/schemas';
+import { Card, CardHeader, CardContent } from '@/components/ui/card';
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormDescription,
+	FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { useCurrentUser } from '@/hooks/use-current-user';
+import { FormSuccess } from '@/components/form-success';
+import { FormError } from '@/components/form-error';
 
 const SettingPage = () => {
-	// const session = await auth(); // on server side with async page
-	// const session = useSession(); // on client side
 	const user = useCurrentUser();
 
-	const onClick = () => {
-		// signOut(); // on client side
-		logout();
+	const [error, setError] = useState<string | undefined>(undefined);
+	const [success, setSuccess] = useState<string | undefined>(undefined);
+	const [isPending, startTransition] = useTransition();
+	const { update } = useSession();
+
+	const form = useForm<z.infer<typeof SettingsSchema>>({
+		resolver: zodResolver(SettingsSchema),
+		defaultValues: {
+			name: user?.name || undefined,
+		},
+	});
+
+	const onSubmit = (values: z.infer<typeof SettingsSchema>) => {
+		startTransition(() => {
+			settings(values)
+				.then((data) => {
+					if (data.error) {
+						setError(data.error);
+					}
+
+					if (data.success) {
+						update();
+						setSuccess(data.success);
+					}
+				})
+				.catch(() => setError('Something went wrong!'));
+		});
 	};
 
 	return (
-		<>
-			{/* <form
-		action={async () => {
-			'use server';
-			await signOut();
-		}}>
-		<button type='submit'>Sign out</button>
-	</form> */}
-
-			<div className='bg-white p-10 rounded-xl'>
-				<button type='submit' onClick={onClick}>
-					Sign out
-				</button>
-			</div>
-		</>
+		<Card className='w-[600px]'>
+			<CardHeader>
+				<p className='text-2xl font-semibold text-center'>⚙️ Settings</p>
+			</CardHeader>
+			<CardContent>
+				<Form {...form}>
+					<form className='space-y-6' onSubmit={form.handleSubmit(onSubmit)}>
+						<div className='space-y-4'>
+							<FormField
+								control={form.control}
+								name='name'
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Name</FormLabel>
+										<FormControl>
+											<Input
+												{...field}
+												placeholder='John Doe'
+												disabled={isPending}
+											/>
+										</FormControl>
+									</FormItem>
+								)}
+							/>
+						</div>
+						<FormError message={error} />
+						<FormSuccess message={success} />
+						<Button type='submit' disabled={isPending}>
+							Save
+						</Button>
+					</form>
+				</Form>
+			</CardContent>
+		</Card>
 	);
 };
 
