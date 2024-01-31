4cdf79a4f9187d1e2d45a115d70c1fe67621cd31
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2024-01-31 14:42:19 +0100

    feat: settings page

--- actions/settings.ts
+++ actions/settings.ts
@@ -2,10 +2,13 @@
 
 import * as z from 'zod';
 
+import bcryptjs from 'bcryptjs';
 import { db } from '@/lib/db';
 import { SettingsSchema } from '@/schemas';
-import { getUserById } from '@/data/user';
+import { getUserByEmail, getUserById } from '@/data/user';
 import { currentUser } from '@/lib/auth';
+import { generateVerificationToken } from '@/lib/tokens';
+import { sendVerificationEmail } from '@/lib/mail';
 
 export const settings = async (values: z.infer<typeof SettingsSchema>) => {
 	const user = await currentUser();
@@ -18,6 +21,46 @@ export const settings = async (values: z.infer<typeof SettingsSchema>) => {
 
 	if (!dbUser) {
 		return { error: 'Unauthorized!' };
+	}
+
+	if (user.isOAuth) {
+		values.email = undefined;
+		values.password = undefined;
+		values.newPassword = undefined;
+		values.isTwoFactorEnabled = undefined;
+	}
+
+	if (values.email && values.email !== user.email) {
+		const existingUser = await getUserByEmail(values.email);
+
+		if (existingUser && existingUser.id !== user.id) {
+			return { error: 'Email already in use!' };
+		}
+
+		const verificationToken = await generateVerificationToken(values.email);
+
+		await sendVerificationEmail(
+			verificationToken.email,
+			verificationToken.token
+		);
+
+		return { success: 'Verification email sent!' };
+	}
+
+	if (values.password && values.newPassword && dbUser.password) {
+		const passwordsMatch = await bcryptjs.compare(
+			values.password,
+			dbUser.password
+		);
+
+		if (!passwordsMatch) {
+			return { error: 'Incorrect password!' };
+		}
+
+		const hashedPassword = await bcryptjs.hash(values.newPassword, 10);
+
+		values.password = hashedPassword;
+		values.newPassword = undefined;
 	}
 
 	await db.user.update({
--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -19,10 +19,19 @@ import {
 	FormDescription,
 	FormMessage,
 } from '@/components/ui/form';
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from '@/components/ui/select';
+import { Switch } from '@/components/ui/switch';
 import { Input } from '@/components/ui/input';
 import { useCurrentUser } from '@/hooks/use-current-user';
 import { FormSuccess } from '@/components/form-success';
 import { FormError } from '@/components/form-error';
+import { UserRole } from '@prisma/client';
 
 const SettingPage = () => {
 	const user = useCurrentUser();
@@ -35,7 +44,12 @@ const SettingPage = () => {
 	const form = useForm<z.infer<typeof SettingsSchema>>({
 		resolver: zodResolver(SettingsSchema),
 		defaultValues: {
+			newPassword: undefined,
+			password: undefined,
 			name: user?.name || undefined,
+			email: user?.email || undefined,
+			role: user?.role || undefined,
+			isTwoFactorEnabled: user?.isTwoFactorEnabled || undefined,
 		},
 	});
 
@@ -78,9 +92,116 @@ const SettingPage = () => {
 												disabled={isPending}
 											/>
 										</FormControl>
+										<FormMessage />
 									</FormItem>
 								)}
 							/>
+							{user?.isOAuth === false && (
+								<>
+									<FormField
+										control={form.control}
+										name='email'
+										render={({ field }) => (
+											<FormItem>
+												<FormLabel>Email</FormLabel>
+												<FormControl>
+													<Input
+														{...field}
+														placeholder='john.doe@example.com'
+														type='email'
+														disabled={isPending}
+													/>
+												</FormControl>
+												<FormMessage />
+											</FormItem>
+										)}
+									/>
+
+									<FormField
+										control={form.control}
+										name='password'
+										render={({ field }) => (
+											<FormItem>
+												<FormLabel>Password</FormLabel>
+												<FormControl>
+													<Input
+														{...field}
+														placeholder='******'
+														type='password'
+														disabled={isPending}
+													/>
+												</FormControl>
+												<FormMessage />
+											</FormItem>
+										)}
+									/>
+									<FormField
+										control={form.control}
+										name='newPassword'
+										render={({ field }) => (
+											<FormItem>
+												<FormLabel>New Password</FormLabel>
+												<FormControl>
+													<Input
+														{...field}
+														placeholder='******'
+														type='password'
+														disabled={isPending}
+													/>
+												</FormControl>
+												<FormMessage />
+											</FormItem>
+										)}
+									/>
+								</>
+							)}
+							<FormField
+								control={form.control}
+								name='role'
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Role</FormLabel>
+										<Select
+											disabled={isPending}
+											onValueChange={field.onChange}
+											defaultValue={field.value}>
+											<FormControl>
+												<SelectTrigger>
+													<SelectValue placeholder='Select a role' />
+												</SelectTrigger>
+											</FormControl>
+											<SelectContent>
+												<SelectItem value={UserRole.ADMIN}>Admin</SelectItem>
+												<SelectItem value={UserRole.USER}>User</SelectItem>
+											</SelectContent>
+										</Select>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+							{user?.isOAuth === false && (
+								<FormField
+									control={form.control}
+									name='isTwoFactorEnabled'
+									render={({ field }) => (
+										<FormItem className='flex items-center justify-between rounded-lg border p-3 shadow-sm'>
+											<div className='space-y-0.5'>
+												<FormLabel>Two Factor Authentication</FormLabel>
+												<FormDescription>
+													Enable two factor authentication for your account
+												</FormDescription>
+											</div>
+											<FormControl>
+												<Switch
+													disabled={isPending}
+													checked={field.value}
+													onCheckedChange={field.onChange}
+												/>
+											</FormControl>
+										</FormItem>
+									)}
+								/>
+							)}
 						</div>
 						<FormError message={error} />
 						<FormSuccess message={success} />
